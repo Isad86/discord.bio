53c7a14e5c181f0fe64a85cd93734cfde3ab6de1
x2sadddDM <114662157+Isad86@users.noreply.github.com>
2024-06-17 12:09:26 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,58 @@ const BADGES = [
   {
     name: "Not The Best Dev But Im the best friend for you", // Message that appears when you hover over it
     image: "assets/badges/bug_hunter_level2.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
-  }
+  },
+  
+  {
+    name: "Exclusive is my freind server not mine", // Message that appears when you hover over it
+    image: "assets/badges/boost24month.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  
+  {
+    name: "i can be mod if you want me to", // Message that appears when you hover over it
+    image: "assets/badges/discord_certified_moderator.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  
+  {
+    name: "i can be your server partner if you want", // Message that appears when you hover over it
+    image: "assets/badges/discord_partner.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  
+  {
+    name: "i can be your server staff if you want", // Message that appears when you hover over it
+    image: "assets/badges/discord_staff.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  
+  {
+    name: "i can be your server nitro gifter if you want", // Message that appears when you hover over it
+    image: "assets/badges/early_supporter.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  
+  {
+    name: "hypesquad_balance", // Message that appears when you hover over it
+    image: "assets/badges/hypesquad_balance.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  {
+    name: "hypesquad_bravery", // Message that appears when you hover over it
+    image: "assets/badges/hypesquad_bravery.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  {
+    name: "hypesquad_brilliance", // Message that appears when you hover over it
+    image: "assets/badges/hypesquad_brilliance.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  {
+    name: "hypesquad_event", // Message that appears when you hover over it
+    image: "assets/badges/hypesquad_event.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  {
+    name: "nitro", // Message that appears when you hover over it
+    image: "assets/badges/nitro.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  {
+    name: "verified_bot_developer", // Message that appears when you hover over it
+    image: "assets/badges/verified_bot_developer.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
+  },
+  
 ];
 
 const USER_ID = "1179227524849475655"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
